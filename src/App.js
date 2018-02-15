56cdbedb1d1a90c0bdf07b43472b9cb3c1383fb5
Thiago_kauai <html5ephp@gmail.com>
2018-02-14 23:14:29 -0200

    criado o comment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import 'bootstrap-css-only'
 import NewComent from './NewComent'
+import Coment from './Comment'
 
 class App extends Component {
 
@@ -9,6 +10,7 @@ class App extends Component {
             <div className="container">
                 <h1 className="text-center alert alert-success">Defininfo o app</h1>
                 <NewComent/>
+                <Coment/>
             </div>
         )
     }
---
+++ src/Comment.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Coments extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Renderizar comentarios aqui!!!</p>
+            </div>
+            
+        );
+    }
+}
+
+export default Coments;
